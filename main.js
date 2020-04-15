a7d6053a3b2c9e8fcfa7fd6386c7eba9f89a82d8
Samarinneo <54540423+Samarinneo@users.noreply.github.com>
2020-04-15 20:28:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    $('.slider').slick({
+    	arrows:false,
+    	infinite: true,
+    	  autoplay: true,
+  autoplaySpeed: 500,
+    fade: true,
+  cssEase: 'linear'
+    });
+});
+
+
+var nisan = document.getElementById('nisan');
+nisan.onclick = function () {
+	  var text = document.querySelector('.typed_nisan');
+	  text.classList.add( 'block' );
+	  	var text = document.querySelector('.typed_ford');
+	  text.classList.add(  'none' );
+	  var text = document.querySelector('.typing');
+	  text.classList.add(  'none' );
+ }
