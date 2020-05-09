8a466c07f7541daf28e00f6aaef74cf5dcdff15d
iedenis <denis.ievlev@gmail.com>
2020-05-10 00:25:10 +0300

    fixing bugs

--- main.js
+++ main.js
@@ -3,19 +3,8 @@ $( document ).ready(function() {
     	arrows:false,
     	infinite: true,
     	  autoplay: true,
-  autoplaySpeed: 1000,
+  autoplaySpeed: 2000,
     fade: true,
   cssEase: 'linear'
     });
 });
-
-
-var nisan = document.getElementById('nisan');
-nisan.onclick = function () {
-	  var text = document.querySelector('.typed_nisan');
-	  text.classList.add( 'block' );
-	  	var text = document.querySelector('.typed_ford');
-	  text.classList.add(  'none' );
-	  var text = document.querySelector('.typing');
-	  text.classList.add(  'none' );
- }
